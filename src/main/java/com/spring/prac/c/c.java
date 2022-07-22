a31010a5e7fb17b1aea6f90f0c4d0949c423a1f2
kkkmmmyyyy <cityray@naver.com>
2022-07-22 18:49:03 +0900

    adasdad

--- src/main/java/com/spring/prac/c/c.java
+++ src/main/java/com/spring/prac/c/c.java
@@ -1,5 +1,13 @@
 package com.spring.prac.c;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class c {
 
+	@GetMapping("ads")
+	public String ssss() {
+		return "aaa";
+	}
 }
